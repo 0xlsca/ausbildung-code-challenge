41c0d152f0cdd20a69ec8530bba8d380cad5620b
Leon Scharfenort <l.dapo@gmx.net>
2020-03-04 00:00:07 +0100

    Added basic table controls

--- js/main.js
+++ js/main.js
@@ -26,16 +26,20 @@ function disableFileInput() {
 
 function createTable() {
   // fills the content div with the table
-  content.append("<div id='csv-table'></div>");
 
   // make tabulator
   table = new Tabulator("#csv-table", {
+    addRowPos: "top",
+    history: true,
     height: $(window).height() * 0.9, // maximum height without too much overhead
     data: tableData.data, //assign data to table
     layout: "fitColumns", //fit columns to width of table (optional)
     columns: makeColumns(tableData.meta.fields),
     pagination: "local",
   });
+
+  // make table controls visible
+  $("#csv-table-controls").show()
 
 }
 
@@ -44,7 +48,7 @@ function makeColumns(header) {
 
   let ret = [];
 
-  Array.from(header).forEach(column => ret.push({title:column, field:column, editor:true }));
+  Array.from(header).forEach(column => ret.push({title: column, field: column, editor: true}));
 
   return ret;
 
@@ -63,9 +67,35 @@ function receiveInput(results, file) {
 }
 
 function showLoadingScreen() {
-
+  // TODO
 }
 
+function setupTableControls() {
+  //Add row on "Add Row" button click
+  $("#csv-table-add-row").click(function () {
+    table.addRow({});
+  });
 
+  //Delete row on "Delete Row" button click
+  $("#csv-table-del-row").click(function () {
+    table.deleteRow(1);
+  });
+
+  //Clear table on "Empty the table" button click
+  $("#csv-table-clear").click(function () {
+    table.clearData()
+  });
+
+  //Reset table contents on "Reset the table" button click
+  $("#csv-table-reset").click(function () {
+    table.setData(tableData.data);
+  });
+
+  // opens a download prompt which exports the currently held data to the downloaded file
+  $("#csv-table-download").click(function () {
+    table.download("csv", inputFile.name);
+  });
+}
 
 fileInput.change(handleNewFile);
+setupTableControls();
